41af9ab79153eaf29de73b4f7e7075191c18b37b
anthony koenig <anthony.kng.pro@gmail.com>
2024-10-25 11:03:54 +0200

    feat(flashMessages): added a timeout to clear the flash messages after 8 seconds

---
+++ assets/controllers/displayFlash_controller.js
@@ -0,0 +1,20 @@
+import { Controller } from '@hotwired/stimulus';
+
+export default class extends Controller {
+    connect() {
+        setTimeout(() => {
+            this.fadeOut();
+        }, 8000);
+    }
+
+    fadeOut() {
+        
+        this.element.style.transition = "opacity 1s";
+        this.element.style.opacity = "0";
+
+        // Supprime l'élément du DOM après la transition
+        this.element.addEventListener("transitionend", () => {
+            this.element.remove();
+        });
+    }
+}
