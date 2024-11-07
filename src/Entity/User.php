0b6327a5337afbfe697f0d458b020434e93da714
anthony koenig <anthony.kng.pro@gmail.com>
2024-11-07 09:17:45 +0100

    fix(messages): fixed messages from forms

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -16,7 +16,7 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
-#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
+#[UniqueEntity(fields: ['email'], message: 'Un compte existe déja avec cette adresse email.')]
 #[ORM\HasLifecycleCallbacks]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
